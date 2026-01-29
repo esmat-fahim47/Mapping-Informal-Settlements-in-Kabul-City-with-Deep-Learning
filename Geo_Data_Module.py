d878905674a2fa3859f58e44f793bff80f1e5c00
esmat-fahim47 <esmat.tariq4747@gmail.com>
2026-01-29 17:55:16 +0100

    Add files via upload
    This is the GeoDataModule for prediction on large images

---
+++ Geo_Data_Module.py
@@ -0,0 +1,24 @@
+from torchgeo.datamodules import NonGeoDataModule
+from torchgeo.datasets import RasterDataset
+from torchgeo.samplers import GridGeoSampler
+from torch.utils.data import DataLoader
+
+class GeoDataModule(NonGeoDataModule):
+    """GeoDataModule for tiled inference on a single large GeoTIFF."""
+
+    def __init__(self, big_image_path: str, batch_size: int = 1, tile_size: int = 512, stride: int = 256, num_workers: int = 0):
+        super().__init__(RasterDataset, batch_size=batch_size, num_workers=num_workers, root=None)
+        self.big_image_path = big_image_path
+        self.tile_size = tile_size
+        self.stride = stride
+        self.num_workers = num_workers
+
+    def predict_dataloader(self):
+        dataset = RasterDataset(self.big_image_path)
+        sampler = GridGeoSampler(dataset, size=self.tile_size, stride=self.stride)
+        return DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            sampler=sampler,
+            num_workers=self.num_workers,
+        )
